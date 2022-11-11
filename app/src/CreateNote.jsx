6380e1f8b40b0d34408f349ec8a33ae40b48211f
Saad575757 <smartysaad59@gmail.com>
2022-11-12 02:10:14 +0500

    Website works

--- app/src/CreateNote.jsx
+++ app/src/CreateNote.jsx
@@ -1,19 +1,55 @@
-import React from "react";
-import Button from '@mui/material/Button';
-import AddIcon from '@mui/icons-material/Add';
+import React, { useState } from "react";
+import './CreateNote.css';
 
-const CreateNote = () =>{
-    return(
+const CreateNote = (props) => {
+    const [note, setnote] = useState({ //note is current data and setnote updated data.
+        title: '',// title and content is an object, title is an imput field
+        content: '',//and content is textarea field, where we have to write data from
+                     //and pass data to title and content and update previous value.
+                     //and whatevere in usestate property, we can call that initial data.
+                     //whatever user writes on input field, inputevent helps to fire data 
+                    //on the current note.
+                    //whaever usestate has value setnote can access.  
+    });
+    const InputEvent = (event) => {
+        // const value = event.target.value;
+        // const name = event.target.name;
+        const {name, value} = event.target; // input event targets two things value and name.
+
+
+        setnote((prevData) => {
+            return{
+                ...prevData,
+                [name]: value,
+            };
+
+        });
+        console.log(note);
+    };
+
+    const addEvent = () => {
+        props.passNote(note);
+        setnote({ //note is current data and setnote updated data.
+            title: '',// title and content is an object, title is an imput field
+            content: '',//and content is textarea field, where we have to write data from
+                         //and pass data to title and content and update previous value.
+                         //and whatevere in usestate property, we can call that initial data.
+                         //whatever user writes on input field, inputevent helps to fire data 
+                        //on the current note.
+                        //whaever usestate has value setnote can access.  
+        });
+
+    };
+
+    return (
         <>
-        <div className="main_note">
-        <form>
-            <input type="text" placeholder="Title" />
-            <textarea  cols="30" rows="10" placeholder="Write a note..."></textarea>
-            <Button>
-                <AddIcon className="plus_sign"/>
-            </Button>
-        </form>
-        </div>
+            <div className="main_note">
+                <form className="form">
+                    <input type="text" placeholder="Title" name="title" value={note.title} onChange={InputEvent} className="text_input" autoComplete="off"></input>
+                    <textarea cols="30" rows="10" placeholder="Write a note..." name="content" value={note.content} onChange={InputEvent} className="textarea"></textarea>
+                    <button className="button" onClick={addEvent}>Add</button>
+                </form>
+            </div>
         </>
     );
 };
---
+++ app/src/Note.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './Note.css';
+
+const Note =(props)=>{
+    return(
+        <>
+        <div className="note">
+            <h1 className="heading">{props.title}</h1>
+            <br />
+            <p className="content">
+                {props.content}
+            </p>
+            <button className="button">Delete</button>
+        </div>
+        </>
+    );
+};
+
+export default Note;
